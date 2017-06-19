6a39a80814deebd5a356755e1255e0c837ec39e7
Tim Hopper <tdhopper@gmail.com>
2017-06-19 11:02:44 -0400

    ready to rock

--- render.py
+++ render.py
@@ -1,8 +1,18 @@
 import json
-import pandas as pd
 from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader
 
+
+packages = {
+    "ggplot": "ggplot2 (R)",
+    "pandas": "Pandas",
+}
+
+names = {
+    "bar-counts": "Basic Bar Chart",
+    "simple-histogram": "Basic Histogram",
+    "scatter-plot": "Basic Scatter Plot",
+}
 
 def get_source_and_image(num, cells):
     image = cells[num]['outputs'][0]['data']['image/png'].replace("\n", "").strip()
@@ -12,10 +22,7 @@ def get_source_and_image(num, cells):
     return source, image
 
 
-
-
-if __name__ == '__main__':
-
+def extract_cells():
     with open("./ggplot vs Python Plotting.ipynb", 'r') as f:
         nb = json.load(f)
     cells = nb['cells']
@@ -26,20 +33,31 @@ if __name__ == '__main__':
         if 'ex' not in tags:
             continue
         tags = {t.split(":")[0]: t.split(":")[1] for t in tags if ":" in t}
-        print(tags['name'])
-        meta[tags['name']].append({
-            "cell_num": cell_num,
-            "package": tags["package"],
-        })
 
-    output = [
-        ["Bar Plot of Counts", "ggplot", *get_source_and_image(6, cells)],
-        ["Bar Plot of Counts", "Pandas", *get_source_and_image(7, cells)]
-    ]
+        source = "".join(cells[cell_num]['source']).strip()
+        if "%R" in source:
+            source = '\n'.join(source.split('\n')[1:])
+        image = cells[cell_num]['outputs'][0]['data']['image/png'].replace("\n", "").strip()
+
+        meta[(names.get(tags['name'], tags['name']), tags['name'])].append({
+            "cell_num": cell_num,
+            "package": packages.get(tags["package"], tags["package"]),
+            "package-slug": tags['package'],
+            "image": image,
+            "content": source,
+        })
+    meta = {k: v[::-1] for k, v in meta.items()}
+    return meta
+
+
+output = defaultdict(list)
+
+if __name__ == '__main__':
+    plots = extract_cells()
 
     env = Environment(loader=FileSystemLoader('web'), extensions=['jinja2_highlight.HighlightExtension'])
     template = env.get_template('t_index.html')
-    output_from_parsed_template = template.render(foo=output)
+    output_from_parsed_template = template.render(foo=output, plots=plots)
 
 
     # to save the results
