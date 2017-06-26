f3dc0e577261a0f41ccbef09e2b0a0857139908d
Tim Hopper <tdhopper@gmail.com>
2017-06-26 10:46:27 -0400

    reset cache after deploy

--- .travis/reset_cloudfront_cache.sh
+++ .travis/reset_cloudfront_cache.sh
@@ -1,6 +1,6 @@
 ###############################################################################
 ###  Resets CloudFront cache with boto/cfadmin utility
-###  Run: ./this_script  
+###  Run: ./reset_cloudfront_cache.sh
 ###############################################################################
 
 #
@@ -20,32 +20,9 @@ else
     exit 0
 fi
 
-#
-# Install boto
-#
-echo -e "\nInstalling boto...\n"
-git clone git://github.com/boto/boto.git
-cd boto
-sudo python setup.py install
-cd ../
-rm -rf boto
-
-#
-# Set up credentials for boto
-#
-echo -e "\nSet up boto credentials...\n"
-cat > ~/.boto < 
-# XNXNXXNNNNXNXN - distribution configured for the Web - in aws amazon cloudfront distributions.
-#
 echo -e "\nCloudFront Invalidating...\n"
 cfadmin invalidate XNXNXXNNNNXNXN /yoursite.com
 cfadmin invalidate XNXNXXNNNNXNXN /.yoursite.com
 cfadmin invalidate XNXNXXNNNNXNXN /index.html /static/main.css /static/main.js
 echo -e "\nInvalidating is in progress...\n"
 echo -e "\nYou can check the status on the 'Invalidations' tab here https://console.aws.amazon.com/cloudfront/home?region=your_region#distribution-settings:XNXNXXNNNNXNXN\n"
-
-#
-# Clean up
-#
-echo -e "\nRemove boto config file\n"
-rm ~/.boto
