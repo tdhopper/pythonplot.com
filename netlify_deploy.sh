143ccec00c24de4384b14a5a30e79f6da4023d2d
Tim Hopper <thopper@cylance.com>
2018-10-18 17:17:49 +0000

    netlify deploy all branches

---
+++ netlify_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $TRAVIS_BRANCH = "foo" ]; then
+    netlify deploy \ 
+        --auth $NETLIFY_AUTH_TOKEN \
+        --site $NETLIFY_SITE_ID \
+        --dir ./web \
+        --prod \
+        --message "Production deploy from Travis CI"
+
+else 
+    netlify deploy \ 
+        --auth $NETLIFY_AUTH_TOKEN \
+        --site $NETLIFY_SITE_ID \
+        --dir ./web \
+        --message "Preview deploy from Travis CI"
+if
