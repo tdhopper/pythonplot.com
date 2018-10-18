066678b9cc84819b60c655de01b65cdaf79abe77
Tim Hopper <thopper@cylance.com>
2018-10-18 17:27:22 +0000

    fix conda env for dploy

--- .travis/install_conda.sh
+++ .travis/install_conda.sh
@@ -1,7 +1,9 @@
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 rm -f miniconda.sh
 hash -r
 $HOME/miniconda/bin/conda config --set always_yes yes --set changeps1 no
 $HOME/miniconda/bin/conda update -q conda
 $HOME/miniconda/bin/conda info -a
+$HOME/miniconda/bin/conda install --quiet pip
+$HOME/miniconda/bin/conda install --quiet --channel r mro-base r-ggplot2
