76056ca6c8a82ee2b0e50b330b364d76753dd377
Tim Hopper <thopper@cylance.com>
2018-10-19 18:06:12 +0000

    remove bad trailing space

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $TRAVIS_BRANCH = "foo" ]; then
-    netlify deploy \ 
+    netlify deploy \
         --auth $NETLIFY_AUTH_TOKEN \
         --site $NETLIFY_SITE_ID \
         --dir ./web \
         --prod \
         --message "Production deploy from Travis CI"
 else 
-    netlify deploy \ 
+    netlify deploy \
         --auth $NETLIFY_AUTH_TOKEN \
         --site $NETLIFY_SITE_ID \
         --dir ./web \
