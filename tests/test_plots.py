6a517b2d066e06b9c077264e01cb6f8432b04d6f
salbaroudi <sean.al.baroudi@gmail.com>
2019-04-10 17:37:55 -0400

    Added more charts.

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,22 +1,22 @@
 import render
 
 defined_plots = {'bar-counts': ['pandas', 'plotnine', 'ggplot', 'plotly', 'altair', ],
-                 'dodged-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
+                 'dodged-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', 'altair', ],
+                 'scatter-plot': ['pandas', 'plotnine', 'ggplot', 'plotly', 'altair',],
                  'scatter-plot-with-colors': ['matplotlib',
                                               'seaborn',
                                               'plotnine',
                                               'ggplot',
-                                              'plotly', ],
-                 'scatter-plot-with-facet': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot-with-facets': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot-with-size': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
+                                              'plotly', 'altair',],
+                 'scatter-plot-with-facet': ['seaborn', 'plotnine', 'ggplot', 'plotly', 'altair',],
+                 'scatter-plot-with-facets': ['seaborn', 'plotnine', 'ggplot', 'plotly', 'altair',],
+                 'scatter-plot-with-size': ['pandas', 'plotnine', 'ggplot', 'plotly', 'altair',],
                  'scatter-with-regression': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'simple-histogram': ['pandas', 'matplotlib', 'plotnine', 'ggplot', 'plotly', ],
-                 'stacked-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
+                 'simple-histogram': ['pandas', 'matplotlib', 'plotnine', 'ggplot', 'plotly', 'altair',],
+                 'stacked-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', 'altair', ],
                  'stacked-kde': ['pandas', 'seaborn', 'plotnine', 'ggplot', 'plotly', ],
                  'stacked-smooth-line-and-scatter': ['plotnine', 'ggplot', 'plotly', ],
-                 'timeseries': ['pandas', 'plotnine', 'ggplot', 'plotly', ]}
+                 'timeseries': ['pandas', 'plotnine', 'ggplot', 'plotly','altair', ]}
 
 
 def test_exist():
