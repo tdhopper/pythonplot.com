d1485aef03940b77172b3244eab61a7fe379bc46
Roy Keyes <roy.coding@gmail.com>
2019-12-22 00:36:24 -0600

    Updated requirements and build files. Formatted test_plots.py with black.

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,26 +1,68 @@
 import render
 
-defined_plots = {'bar-counts': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'dodged-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot-with-colors': ['matplotlib',
-                                              'seaborn',
-                                              'plotnine',
-                                              'ggplot',
-                                              'plotly', ],
-                 'scatter-plot-with-facet': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot-with-facets': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-plot-with-size': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'scatter-with-regression': ['seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'simple-histogram': ['pandas', 'matplotlib', 'plotnine', 'ggplot', 'plotly', ],
-                 'stacked-bar-chart': ['pandas', 'plotnine', 'ggplot', 'plotly', ],
-                 'stacked-kde': ['pandas', 'seaborn', 'plotnine', 'ggplot', 'plotly', ],
-                 'stacked-smooth-line-and-scatter': ['plotnine', 'ggplot', 'plotly', ],
-                 'timeseries': ['pandas', 'plotnine', 'ggplot', 'plotly', ]}
+defined_plots = {
+    "bar-counts": ["pandas", "plotnine", "ggplot", "plotly", "altair",],
+    "dodged-bar-chart": ["pandas", "plotnine", "ggplot", "plotly", "altair",],
+    "scatter-plot": ["pandas", "plotnine", "ggplot", "plotly", "altair",],
+    "scatter-plot-with-colors": [
+        "matplotlib",
+        "seaborn",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "scatter-plot-with-facet": [
+        "seaborn",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "scatter-plot-with-facets": [
+        "seaborn",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "scatter-plot-with-size": [
+        "pandas",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "scatter-with-regression": ["seaborn", "plotnine", "ggplot", "plotly",],
+    "simple-histogram": [
+        "pandas",
+        "matplotlib",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "stacked-bar-chart": ["pandas", "plotnine", "ggplot", "plotly", "altair",],
+    "stacked-kde": [
+        "pandas",
+        "seaborn",
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "stacked-smooth-line-and-scatter": [
+        "plotnine",
+        "ggplot",
+        "plotly",
+        "altair",
+    ],
+    "timeseries": ["pandas", "plotnine", "ggplot", "plotly", "altair",],
+}
 
 
 def test_exist():
     plots = render.extract_cells("Examples.ipynb")
     for name, slug in plots:
-        names = [d['package-slug'] for d in plots[(name, slug)]]
+        names = [d["package-slug"] for d in plots[(name, slug)]]
         assert set(names) == set(defined_plots[slug])
